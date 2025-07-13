d0f7e8ea1647d973dbd9d4b008068b011bfbf599
Arina Maslanova <maslanovaa@mail.ru>
2025-07-13 15:39:25 +0300

    test

--- frontend/src/services/websocket.js
+++ frontend/src/services/websocket.js
@@ -19,6 +19,7 @@ export const initWebSocket = (dispatchInstance) => {
           ...data,
           text: filterProfanity(data.text),
         }
+        console.log(filteredData)
         dispatch(addMessage(filteredData))
       }
     })
